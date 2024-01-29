13c0fe2b1bf2a7049f130858cf6c7afa9a90825e
Tenoy Thankachan <113044871+tenoyt@users.noreply.github.com>
2024-01-29 16:22:24 -0500

    Add files via upload

---
+++ src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -0,0 +1,34 @@
+package com.ontariotechu.sofe3980U;
+
+import org.joda.time.LocalTime;
+
+/**
+ * To run the Calculator
+ *
+ */
+public class App 
+{
+	/**
+	* Main program:  The entry point of the program. The local time will be printed first,
+	*      Then it will create two binary variables, add them and print the result.
+	*
+	* @param args: not used
+	*/
+    public static void main( String[] args )
+    {
+		LocalTime currentTime = new LocalTime();
+			System.out.println("The current local time is: " + currentTime);
+		Binary binary1=new Binary("00010001000");
+			System.out.println( "First binary number is "+binary1.getValue());
+		Binary binary2=new Binary("111000");
+        	System.out.println( "Second binary number is "+binary2.getValue());
+		Binary sum= Binary.add(binary1,binary2);
+			System.out.println( "Their summation is "+sum.getValue());
+		Binary Or = Binary.or(binary1,binary2);
+			System.out.println( "Their Or is "+Or.getValue());
+		Binary And = Binary.and(binary1,binary2);
+			System.out.println( "Their And is "+And.getValue());
+		Binary product = Binary.multiply(binary1,binary2);
+			System.out.println( "Their Product is "+product.getValue());
+    }
+}
